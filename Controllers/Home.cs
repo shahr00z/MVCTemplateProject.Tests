442e5da79346f4e43cb220f4091043a6a6ff568c
Shahrooz Jafari <sh.mjsoft@gmail.com>
2012-11-11 23:03:19 +0330

    FirstCommit

---
+++ Controllers/Home.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using DomainModel;
+using MVCTemplateProject.Controllers;
+using Moq;
+using NUnit.Framework;
+
+namespace MVCTemplateProject.Tests.Controllers
+{
+    [TestFixture]
+    public class Home
+    {
+        [Test]
+        public void IndxShouldReturnListOfPage()
+        {
+
+        }
+    }
+}
