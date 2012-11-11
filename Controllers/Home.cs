fe42afcead3ae1e0a3812d451e852ad5e10f8d8f
Shahrooz Jafari <sh.mjsoft@gmail.com>
2012-11-12 00:37:10 +0330

    First Test

--- Controllers/Home.cs
+++ Controllers/Home.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Web.Mvc;
-using DomainModel;
+using Business;
 using MVCTemplateProject.Controllers;
 using Moq;
 using NUnit.Framework;
+using Post = DomainModel.Post;
 
 namespace MVCTemplateProject.Tests.Controllers
 {
@@ -15,7 +17,25 @@ namespace MVCTemplateProject.Tests.Controllers
         [Test]
         public void IndxShouldReturnListOfPage()
         {
+          
+            var posts = new List<Post>
+                            {
+                                new Post {Name = "Test"},
+                                new Post {Name = "Test"},
+                                new Post {Name = "Test"},
+                                new Post {Name = "Test"}
+                            };
+          
+            var mockRepository = new Mock<IBusinessContext>();
+            mockRepository.Setup(mr => mr.Post.List(It.IsAny<Expression<Func<Post, bool>>>())).Returns(posts.ToList());
+            mockRepository.SetupGet(p=>p.Post).Returns(mockRepository.Object.Post);
 
+            var controller = new HomeController(mockRepository.Object);
+            var result = controller.Index() as ViewResult;
+            var model = result.Model as List<Post>;
+
+            Assert.AreEqual(model.Count, 4);
+            Assert.AreEqual(string.Empty,result.ViewName);
         }
     }
 }
