6c9b0b08ecdb3d940a3e8cc4727952a00471bd9e
Shahrooz Jafari <sh.mjsoft@gmail.com>
2012-11-26 20:44:52 +0330

    1

--- Controllers/Home.cs
+++ Controllers/Home.cs
@@ -1,19 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web.Mvc;
 using Data;
 using DomainModel;
 using MVCTemplateProject.Controllers;
 using Moq;
 using NUnit.Framework;
-using ServiceLayer;
+using ServiceLayer.EfServices;
 
 namespace MVCTemplateProject.Tests.Controllers
 {
     [TestFixture]
     public class Home
     {
+        #region Setup/Teardown
+
+        [SetUp]
+        public void Setup()
+        {
+            _unitOfWork = new Mock<EfContext>();
+            _services = new Mock<ServiceLayer.Services>(_unitOfWork.Object);
+        }
+
+        #endregion
+
+        private Mock<EfContext> _unitOfWork;
+        private Mock<ServiceLayer.Services> _services;
+
         [Test]
         public void IndxShouldReturnListOfPage()
         {
@@ -24,17 +37,13 @@ namespace MVCTemplateProject.Tests.Controllers
                                 new Post {Name = "Test"},
                                 new Post {Name = "Test"}
                             };
-            var efContext = new Mock<EfContext>();
-            var serviceContext = new ServiceLayer.Services(efContext.Object);
-            var post = new ServiceLayer.EfServices.EfPostService(efContext.Object);
 
-            var rep = new Moq.Mock<Services>(efContext);
-            rep.Object.Post = post;
-            rep.Setup(x => x.Post).Returns(post);
-            rep.Setup(x => x.Post.List(It.IsAny<Func<Post, bool>>())).Returns(posts);
-            var controller = new HomeController(efContext.Object);
-            List<Post> model = controller.DataContext.Post.List();
+            _services.Setup(x => x.Post.List(It.IsAny<Func<Post, bool>>())).Returns(posts);
+
+            var controller = new HomeController(_unitOfWork.Object);
             var result = controller.Index() as ViewResult;
+            var model = result.Model as List<Post>;
+
 
             Assert.AreEqual(model.Count, 4);
             Assert.AreEqual(string.Empty, result.ViewName);
