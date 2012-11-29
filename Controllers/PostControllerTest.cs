47307c351e050717c67a478eb14a07d60af8f73c
Shahrooz Jafari <sh.mjsoft@gmail.com>
2012-11-30 01:10:28 +0330

    Develop

--- Controllers/PostControllerTest.cs
+++ Controllers/PostControllerTest.cs
@@ -5,6 +5,8 @@ using System.Web.Mvc;
 using Data;
 using DomainModel;
 using MVCTemplateProject.Controllers;
+using MVCTemplateProject.Mappers;
+using MVCTemplateProject.ViewModel;
 using Moq;
 using NUnit.Framework;
 using ServiceLayer.ServiceDbSet;
@@ -34,25 +36,51 @@ namespace MVCTemplateProject.Tests.Controllers
 		private Mock<IUnitOfWork> UnitOfWork { get; set; }
 
 		[Test]
-		public void IndexShouldShowPostById()
+		public void DetailShouldRedirectToHomePageWhenNotFoundPostOrPostIdIs0()
+		{
+			PostDbSet.Setup(x => x.Get(It.IsAny<Func<Post, bool>>())).Returns(Posts.Find(post => post.Id == 100));
+
+			var redirectToRoutResult =
+				(RedirectToRouteResult) new PostController(UnitOfWork.Object, PostDbSet.Object).Details(100);
+
+			Assert.AreEqual("Index", redirectToRoutResult.RouteValues["action"]);
+			Assert.AreEqual("Home", redirectToRoutResult.RouteValues["Controller"]);
+		}
+
+		[Test]
+		public void DetailShouldShowPostById()
 		{
 			PostDbSet.Setup(x => x.Get(It.IsAny<Func<Post, bool>>())).Returns(Posts.First());
-			var result = PostController.Index(1) as ViewResult;
-			var model = result.Model as Post;
+			var result = PostController.Details(1) as ViewResult;
+			var model = result.Model as PostViewModel;
 
 			Assert.AreNotEqual(null, model);
 			Assert.AreEqual("I Can", model.Title);
 			Assert.AreEqual(string.Empty, result.ViewName);
 		}
+
 		[Test]
-		public void IndexShouldRedirectToHomePageWhenNotFoundPostOrPostIdIs0()
+		public void ShouldCreatePostAndRedirectToDetailsWithPostViewModel()
 		{
-			PostDbSet.Setup(x => x.Get(It.IsAny<Func<Post, bool>>())).Returns(Posts.Find(post => post.Id == 100));
+			var postViewModel = new PostViewModel {Title = "Test", Id = 1, Content = "st"};
+			Post post = new PostMapper().MappViewModelToModel(postViewModel);
 
-			var redirectToRoutResult = (RedirectToRouteResult)new PostController(UnitOfWork.Object, PostDbSet.Object).Index(100);
+			var result = PostController.Create(postViewModel) as RedirectToRouteResult;
 
-			Assert.AreEqual("Index", redirectToRoutResult.RouteValues["action"]);
-			Assert.AreEqual("Home", redirectToRoutResult.RouteValues["Controller"]);
+			PostDbSet.Verify(set => set.Add(It.IsAny<Post>()), Times.Once());
+			Assert.AreEqual("Details", result.RouteValues["action"]);
+		}
+
+		[Test]
+		public void ShouldShowCreatePostWithPostViewModel()
+		{
+			var postViewModel = new PostViewModel {};
+
+			var result = PostController.Create() as ViewResult;
+			var model = result.Model as PostViewModel;
+
+			Assert.AreEqual(string.Empty, result.ViewName);
+			Assert.AreEqual(model,postViewModel);
 		}
 	}
 }
